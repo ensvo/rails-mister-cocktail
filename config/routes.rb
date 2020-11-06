c793bb2b14300931eadade1ca848423531d991b6
Liz Vo <liz_quack@hotmail.com>
2020-11-06 16:16:10 +0900

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'cocktails#index', as: :cocktails
+  root 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
   	resources :doses, only: [:new, :create, :delete]
   end
